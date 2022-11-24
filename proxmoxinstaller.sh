d6444d2a12870fc16530f2ff9ef350bb7f14c2ef
codding-nepale <79444135+codding-nepale@users.noreply.github.com>
2022-11-24 17:40:35 +0100

    Update proxmoxinstaller.sh

--- proxmoxinstaller.sh
+++ proxmoxinstaller.sh
@@ -3,9 +3,12 @@ echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-su
 wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg 
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg 
 apt update -y && apt full-upgrade -y
+apt install pve-kernel-5.15 -y
 apt install proxmox-ve postfix open-iscsi -y
 clear
 echo "installation of the theme in progress..."
 bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
 clear
 echo "install theme end with success and a mandatory restart will be carried out"
+systemctl reboot
+=
